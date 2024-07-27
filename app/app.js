66880103f38ba8cf4f49855c813548b114675621
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-07-27 16:49:05 +0600

    feat: added path function on app js file

--- app/app.js
+++ app/app.js
@@ -1,20 +1,16 @@
+const path = require("path");
 const express = require("express");
-const cors = require("cors");
 const middleware = require("../middleware/middleware");
 const router = require("../routes");
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  credential: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
+// Set 'views' directory for any views being rendered
+app.set("views", path.join(__dirname, "views"));
 
 app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
-app.options("", cors(corsOptions));
 app.use([middleware, router]);
 
 module.exports = app;
