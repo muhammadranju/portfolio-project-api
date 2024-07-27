1a9f467eb585b61581cdd08f8d373de902d55e42
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-07-27 17:49:38 +0600

    update all files

--- controller/project/findAll.js
+++ controller/project/findAll.js
@@ -2,7 +2,7 @@ const Project = require("../../models/project.model");
 
 const findAllProjectController = async (req, res, next) => {
   try {
-    const project = await Project.find();
+    const project = await Project.find().sort({ createdAt: -1 }).exec();
 
     return res.status(200).json(project);
   } catch (error) {
--- models/project.model.js
+++ models/project.model.js
@@ -1,38 +1,41 @@
 const { Schema, model } = require("mongoose");
 
-const projectSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const projectSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    details: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    liveLink: {
+      type: String,
+    },
+    sourceCode: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
   },
-  details: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-  liveLink: {
-    type: String,
-  },
-  sourceCode: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Project = model("Project", projectSchema);
 
